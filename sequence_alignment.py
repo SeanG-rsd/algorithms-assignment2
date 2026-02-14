c6e3f3f61cbac0cb12254935b439860cfc31f371
SeanG <70712778+SeanG-rsd@users.noreply.github.com>
2026-02-14 11:08:06 -0800

    done

--- sequence_alignment.py
+++ sequence_alignment.py
@@ -25,7 +25,7 @@ with open("./imp2cost.txt", "r") as file:
         idx += 1
 
 # reading inputs
-with open("./runtime_test_input.txt", "r") as file:
+with open("./imp2input.txt", "r") as file:
     i = 0
     for line in file:
         inputSeq.append(line.strip('\n').split(','))
